af39358ddc89b1ee98f011965f0e682ce42bd0d9
Minh <hlminh2000@gmail.com>
2017-09-17 15:29:13 -0400

    selection layer implemented

--- src/pixiInterface/components/selectionLayer.js
+++ src/pixiInterface/components/selectionLayer.js
@@ -11,37 +11,50 @@ export default function SelectionLayer(_config){
   const self = this
   const display = new PIXI.Graphics()
   const selectionShape = new PIXI.Graphics()
+  const pointerDownPosition = new PIXI.Point()
 
   const onMouseDown = (e) => {
-    console.log(e)
-    // selectionShape
-    stage.on('mouseup', onMouseUp)
-    stage.on('mousemove', onMouseMove)
+    pointerDownPosition.x = e.data.global.x
+    pointerDownPosition.y = e.data.global.y
+    selectionShape.x = pointerDownPosition.x
+    selectionShape.y = pointerDownPosition.y
+    selectionShape.clear()
+    display.addChild(selectionShape)
+    display.on('pointerup', onMouseUp)
+    display.on('pointermove', onMouseMove)
   }
 
   const onMouseUp = (e) => {
-    console.log(e)
+    display.removeChild(selectionShape)
+    display.off('pointerup', onMouseUp)
+    display.off('pointermove', onMouseMove)
   }
 
   const onMouseMove = (e) => {
-    console.log(e)
+    const selectionWidth = e.data.global.x - pointerDownPosition.x
+    const selectionHeight = e.data.global.y - pointerDownPosition.y
+    selectionShape
+      .clear()
+      .lineStyle(1, 0xffffff, 1)
+      .beginFill(0xffffff, 0.3)
+      .drawRect(0, 0, selectionWidth, selectionHeight)
   }
 
   const updateRenderSize = () => {
     display
       .clear()
-      .beginFill(0x000000, 0.5)
-      .lineStyle(2, 0x000000, 1)
-      .drawRect(0, 0, config.app.renderer.view.width, config.app.renderer.view.height)
+      .beginFill(0x000000, 0)
+      .drawRect(0, 0, config.app.renderer.view.offsetWidth, config.app.renderer.view.offsetHeight)
       .endFill()
   }
 
+  console.log(config.app.renderer);
+
   updateRenderSize()
   window.addEventListener('resize', updateRenderSize)
-  display.on('pointerdown', (e) => {
-    console.log("asdfdfsg");
-    // onMouseDown)()
-  })
+  display.on('pointerdown', onMouseDown)
+  display.interactive = true;
+
 
 
   // public interface
